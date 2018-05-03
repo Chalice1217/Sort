cac544f2b6353551c66182b83655628029446b8c
Chalice1217 <1589748306@qq.com>
2018-05-03 11:07:34 +0800

    快排和插入排序

---
+++ Assets/Scripts/InsertSort.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 插入排序(O(n^2))
+/// 时间复杂度越高,执行效率越低
+/// </summary>
+public class InsertSort : MonoBehaviour
+{
+
+    private void Start()
+    {
+        int[] arr = new int[] { 1, 5, 3, 10, 8, 9, 4, 2, 1, 5, 6 };
+        AscendingSort(arr);
+        foreach (var item in arr)
+        {
+            print(item);
+        }
+    }
+
+    /// <summary>
+    /// 升序
+    /// </summary>
+    /// <param name="arr"></param>
+    private void AscendingSort(int[] arr)
+    {
+        for (int i = 1; i < arr.Length; i++) // 默认第一个数有序,所以i从1开始
+        {
+            int tempArr = arr[i];
+            int index = i - 1;
+            while (index >=0 && tempArr > arr[index]) //  tempArr > arr[index]  大于是从大到小(降序) 
+            {
+                arr[index + 1] = arr[index];
+                index--;
+            }
+            arr[index + 1] = tempArr;
+        }
+    }
+
+    /// <summary>
+    /// 降序
+    /// </summary>
+    /// <param name="arr"></param>
+    private void DescendingSort(int[] arr)
+    {
+        for (int i = 1; i < arr.Length; i++) // 默认第一个数有序,所以i从1开始
+        {
+            int tempArr = arr[i];
+            int index = i - 1;
+            while (index >= 0 && tempArr < arr[index]) //  tempArr > arr[index]   小于是从小到大(升序)
+            {
+                arr[index + 1] = arr[index];
+                index--;
+            }
+            arr[index + 1] = tempArr;
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/QuickSort.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 快速排序
+/// </summary>
+public class QuickSort : MonoBehaviour
+{
+
+    private void Start()
+    {
+        int[] arr = new int[] {1, 11, 5, 3, 10, 8, 9, 4, 2, 1, 5, 6 };
+        QuickSort_Ascending(arr, 0, arr.Length - 1);
+        foreach (var item in arr)
+        {
+            print(item);
+        }
+
+    }
+
+  
+
+    /// <summary>
+    /// 快排升序
+    /// </summary>
+    private void QuickSort_Ascending(int[] arr, int startIndex, int endIndex)
+    {
+        if (startIndex >= endIndex)
+            return;
+
+        int leftIndex = startIndex;
+        int rightIndex = endIndex;
+        int pivotArr = arr[leftIndex]; // 基准数
+
+        while (leftIndex < rightIndex)
+        {
+            // 右边找比基准数大的
+            while (leftIndex < rightIndex && arr[rightIndex] >= pivotArr)
+            {
+                rightIndex--;
+            }
+            // 左边找比基准数小的
+            while (leftIndex < rightIndex && arr[leftIndex] <= pivotArr)
+            {
+                leftIndex++;
+            }
+
+            if (leftIndex < rightIndex)
+            {
+                int temp = arr[leftIndex];
+                arr[leftIndex] = arr[rightIndex];
+                arr[rightIndex] = temp;
+            }
+        }
+
+        // 当leftIndex = rightIndex 时 跳出循环,执行下面
+        // 此时左边都比pivotArr要小 , 右边都比pivotArr 要大 ;
+        // 把基准数归位 ;
+        arr[startIndex] = arr[leftIndex];
+        arr[leftIndex] = pivotArr;
+        // 递归
+        QuickSort_Ascending(arr, startIndex, leftIndex - 1);
+        QuickSort_Ascending(arr, leftIndex + 1, endIndex);
+
+    }
+
+
+    /// <summary>
+    /// 快排降序
+    /// </summary>
+    private void QuickSort_Descending(int[] arr, int startIndex, int endIndex)
+    {
+        if (startIndex >= endIndex) // 递归出口(没有出口就死循环了)
+            return;
+
+        int leftIndex = startIndex;
+        int rightIndex = endIndex;
+        int pivotArr = arr[leftIndex]; // 基准数
+
+        while (leftIndex < rightIndex)
+        {
+            // 右边找比基准数小的
+            while (leftIndex < rightIndex && arr[rightIndex] <= pivotArr)
+            {
+                rightIndex--;
+            }
+            // 左边找比基准数大的
+            while (leftIndex < rightIndex && arr[leftIndex] >= pivotArr)
+            {
+                leftIndex++;
+            }
+
+            if (leftIndex < rightIndex)
+            {
+                int temp = arr[leftIndex];
+                arr[leftIndex] = arr[rightIndex];
+                arr[rightIndex] = temp;
+            }
+        }
+
+        // 当leftIndex = rightIndex 时 跳出循环,执行下面
+        // 此时左边都比pivotArr要大 , 右边都比pivotArr 要小 ;
+        // 把基准数归位 ;
+        arr[startIndex] = arr[leftIndex];
+        arr[leftIndex] = pivotArr;
+        // 递归
+        QuickSort_Ascending(arr, startIndex, leftIndex - 1);
+        QuickSort_Ascending(arr, leftIndex + 1, endIndex);
+
+    }
+
+
+}
